b556d0356b286537e218c633d95fe07674a05fcc
Muhammad Shahbaz <shahbaz.pro2@gmail.com>
2024-05-14 18:31:40 +0500

    Update version

--- dist/cjs/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useOpenCloseModal = exports.useModalState = exports.useGetSetModal = void 0;
-var modal_1 = require("./modal");
-Object.defineProperty(exports, "useGetSetModal", { enumerable: true, get: function () { return modal_1.useGetSetModal; } });
-Object.defineProperty(exports, "useModalState", { enumerable: true, get: function () { return modal_1.useModalState; } });
-Object.defineProperty(exports, "useOpenCloseModal", { enumerable: true, get: function () { return modal_1.useOpenCloseModal; } });
-//# sourceMappingURL=index.js.map
